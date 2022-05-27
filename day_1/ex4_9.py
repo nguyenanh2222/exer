a7f6fe7df83c248e7895eecfbe4320358d91f57d
nguyenanh2222 <nguyenvieanh2103@gmail.com>
2022-05-27 11:38:58 +0700

    [add]: day 3

---
+++ day_1/ex4_9.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+def solve(numbers):
+    """Tìm phần tử lớn nhất của list số nguyên `numbers`
+    Không sử dụng function `max`, `sorted`
+
+    Gợi ý: python có sẵn giá trị âm/dương vô cùng.
+    """
+    assert isinstance(numbers, list)
+    result = None
+
+    # Xoá dòng sau và viết code vào đây set các giá trị phù hợp
+    raise NotImplementedError("Bạn chưa làm bài này")
+
+    return result
+
+
+def main():
+    print(solve([-1, 5, 9, 6, -999999999999999, 1]))
+
+
+if __name__ == "__main__":
+    main()
